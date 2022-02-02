d287d6666d0b6f067331d0c952680464d4416ef6
Praveen Lohar <praveenlohar.in@gmail.com>
2022-02-02 23:00:00 +0530

    updated data.js file

---
+++ data.js
@@ -0,0 +1,469 @@
+const musicData = [
+  {
+    id: "f906c4c504784389a349796186570ca0",
+    albumArtist: "Jaymes Young",
+    album: "Feel Something",
+    albumArt: "./images/infinty.jpeg",
+    albumSongs: [
+      {
+        songName: "Infinity",
+        artist: "Jaymes Young",
+        songUrl:
+          "./songs/Jaymes Young - Infinity [Official Audio].mp3",
+        // songCoverImg: "./images/infinty.jpeg",
+        songCoverImg: "./images/infinty.jpeg",
+      },
+      {
+        songName: "Stone",
+        artist: "Jaymes Young & Phoebe Ryan",
+        songUrl: "./songs/Jaymes Young - Stone [Official Audio].mp3",
+        songCoverImg: "./images/wewont.jpg",
+      },
+      {
+        songName: "Sugar Burn",
+        artist: "Jaymes Young",
+        songUrl:
+          "./songs/Jaymes Young - Sugar Burn [Official Audio].mp3",
+        songCoverImg: "./images/sugarBurn.jpg",
+      },
+      {
+        songName: "Tied Down",
+        artist: "Jaymes Young",
+        songUrl:
+          "./songs/Jaymes Young - Tied Down [Official Audio].mp3",
+        songCoverImg: "./images/tiedDown.jpg",
+      },
+      {
+        songName: "Black Magic",
+        artist: "Jaymes Young",
+        songUrl: "./songs/",
+        songCoverImg: "./images/blackmagic.jpg",
+      },
+    ],
+  },
+  {
+    id: "c4734761d65b11ecab4d33d651ddc544",
+    albumArtist: "Justin Bieber",
+    album: "Justice",
+    albumArt: "./images/justice.jpg",
+    albumSongs: [
+      {
+        songName: "Ghost",
+        artist: "Justin Bieber",
+        songUrl: "./songs/Justin-Bieber-Ghost-(TrendyBeatz.com).mp3",
+        songCoverImg: "./images/ghost.jpg",
+      },
+      {
+        songName: "Deserve You",
+        artist: "Justin Bieber",
+        songUrl:
+          "./songs/Justin-Bieber-Deserve-You-(TrendyBeatz.com).mp3",
+        songCoverImg: "./images/deserveyou.jpg",
+      },
+      {
+        songName: "Off My face",
+        artist: "Justin Bieber",
+        songUrl:
+          "./songs/Justin-Bieber-Off-My-Face-(TrendyBeatz.com).mp3",
+        songCoverImg: "./images/offtheface.jpg",
+      },
+      {
+        songName: "Unstable",
+        artist: "Justin Bieber feat. Kid LAROI",
+        songUrl:
+          "./songs/Justin-Bieber-Ft-The-Kid-LAROI-Unstable-(TrendyBeatz.com).mp3",
+        songCoverImg: "./images/unstable.jpg",
+      },
+      {
+        songName: "Die For You",
+        artist: "Justin Bieber feat. Dominic Fike",
+        songUrl:
+          "./songs/Justin-Bieber-Die-For-You-Ft-Dominic-Fike-(TrendyBeatz.com).mp3",
+        songCoverImg: "./images/dieforyou.jpg",
+      },
+      {
+        songName: "Peaches",
+        artist: "Justin Bieber feat. Daniel Ceaser",
+        songUrl:
+          "./songs/Justin-Bieber-Peaches-Ft-Daniel-Caesar-Giveon-(TrendyBeatz.com).mp3",
+        songCoverImg: "./images/peaches.jpg",
+      },
+    ],
+  },
+  {
+    id: "db9cef0523f84d3680aca9bdfd881619",
+    albumArtist: "Ap Dhillon",
+    album: "Excuses",
+    albumArt: "./images/excuses.jpg",
+    albumSongs: [
+      {
+        songName: "Excuses",
+        artist: "Ap Dhillon",
+        songUrl: "./songs/Excuses - Ap Dhillon.mp3",
+        songCoverImg: "./images/excuses.jpg",
+      },
+      {
+        songName: "Borwn Munde",
+        artist: "Ap Dhillon, Gurinder Gill",
+        songUrl: "./songs/Brown Munde.mp3",
+        songCoverImg: "./images/brownmunde.jpg",
+      },
+      {
+        songName: "Insane",
+        artist: "Ap Dhillon, Gurinder Gill",
+        songUrl: "./songs/",
+        songCoverImg: "./images/insane.jpeg",
+      },
+      {
+        songName: "Desires",
+        artist: "Ap Dhillon,  Shinda Kahlon",
+        songUrl: "./songs/DESIRES - Ap Dhillon.mp3",
+        songCoverImg: "./images/mabelle.jpeg",
+      },
+    ],
+  },
+  {
+    id: "e39d6548e6684f3eace57858793a61e4",
+
+    albumArtist: "Ravi Basrur",
+    album: "KGF Chapter 2",
+    albumArt: "./images/kgf2.jpg",
+    albumSongs: [
+      {
+        songName: "Toofan",
+        artist: "Ravi Basrur",
+        songUrl: "./songs/Toofan - KGF Chapter 2.mp3",
+        songCoverImg: "./images/toofan.jpg",
+      },
+      {
+        songName: "Falak Tu Garaj Tu",
+        artist: "Suchetta Basrur",
+        songUrl: "./songs/Falak Tu Garaj Tu - KGF 2.mp3",
+        songCoverImg: "./images/falak.jpg",
+      },
+      {
+        songName: "Sultan",
+        artist: "Brijesh Shandilya",
+        songUrl: "./songs/Sulthan - KGF 2.mp3",
+        songCoverImg: "./images/sultan.jpg",
+      },
+      {
+        songName: "Mehbooba",
+        artist: "Ananya Bhat",
+        songUrl: "./songs/Mehabooba Hindi - KGF 2.mp3",
+        songCoverImg: "./images/mehbooba.jpeg",
+      },
+    ],
+  },
+  {
+    id: "f5c9ac8068314e5e91462d358ef99434",
+
+    albumArtist: "Ed Sheeran",
+    album: "Divide",
+    albumArt: "./images/divide.jpg",
+    albumSongs: [
+      {
+        songName: "Shape of You",
+        artist: "Ed Sheeran",
+        songUrl: "./songs/Ed Sheeran - Shape Of You.mp3",
+        songCoverImg: "./images/shapeofyou.jpg",
+      },
+      {
+        songName: "Perfect",
+        artist: "Ed Sheeran",
+        songUrl: "./songs/",
+        songCoverImg: "./images/perfect.jpg",
+      },
+      {
+        songName: "Happier",
+        artist: "Ed Sheeran",
+        songUrl: "./songs/Happier-via-Naijafinix.com_.mp3",
+        songCoverImg: "./images/happier.jpg",
+      },
+      {
+        songName: "Drunk",
+        artist: "Ed Sheeran",
+        songUrl: "./songs/Drunk_21.mp3",
+        songCoverImg: "./images/savemyself.jpg",
+      },
+      {
+        songName: "Sunburn",
+        artist: "Ed Sheeran",
+        songUrl: "./songs/Sunburn_24.mp3",
+        songCoverImg: "./images/shapeofyou.jpg",
+      },
+      {
+        songName: "Beautiful People",
+        artist: "Ed Sheeran",
+        songUrl: "./songs/beautiful People (Live At Abbey Road).mp3",
+        songCoverImg: "./images/bibiayeye.jpg",
+      },
+    ],
+  },
+  {
+    id: "ae8429311c684cfe994cf8dd65b667dd",
+
+    albumArtist: "B Praak",
+    album: "Shershaah",
+    albumArt: "./images/shershaah.jpg",
+    albumSongs: [
+      {
+        songName: "Raataan Lambiyan",
+        artist: "Tanishk Bagchi",
+        songUrl: "./songs/Raataan Lambiyan - Shershaah.mp3",
+        songCoverImg: "./images/raatan.jpg",
+      },
+      {
+        songName: "Ranjha",
+        artist: "Jasleen Royal",
+        songUrl: "./songs/Ranjha - Shershaah.mp3",
+        songCoverImg: "./images/ranjha.jpg",
+      },
+      {
+        songName: "Kabhi Tumhe",
+        artist: "Javed - Mohsin, Darshan Raval",
+        songUrl: "./songs/Kabhi Tumhe - Shershaah.mp3",
+        songCoverImg: "./images/kabhitumhe.jpg",
+      },
+      {
+        songName: "Mann Bharryaa 2.0",
+        artist: "B Praak",
+        songUrl: "./songs/Man Bharya 2.mp3",
+        songCoverImg: "./images/mannbharay.jpg",
+      },
+    ],
+  },
+  {
+    id: "redc55bfdbe49689faab2f15e4f4986",
+
+    albumArtist: "Adele",
+    album: "30",
+    albumArt: "./images/thirty.jpg",
+    albumSongs: [
+      {
+        songName: "Strangers By Nature",
+        artist: "Adele",
+        songUrl: "./songs/01 Strangers By Nature.mp3",
+        songCoverImg: "./images/adele_30.jpeg",
+      },
+      {
+        songName: "Easy On Me",
+        artist: "Adele",
+        songUrl: "./songs/02 Easy On Me.mp3",
+        songCoverImg: "./images/adele_30.jpeg",
+      },
+      {
+        songName: "My Little Love",
+        artist: "Adele",
+        songUrl: "./songs/03 My Little Love.mp3",
+        songCoverImg: "./images/adele_30.jpeg",
+      },
+      {
+        songName: "Cry Your Heart Out",
+        artist: "Adele",
+        songUrl: "./songs/04 Cry Your Heart Out.mp3",
+        songCoverImg: "./images/adele_30.jpeg",
+      },
+      {
+        songName: "Oh My God",
+        artist: "Adele",
+        songUrl: "./songs/05 Oh My God.mp3",
+        songCoverImg: "./images/adele_30.jpeg",
+      },
+    ],
+  },
+  {
+    id: "febb2aa106964d0ca3ed0878b1a74825",
+
+    albumArtist: "Sachet Tandon",
+    album: "Kabir Singh",
+    albumArt: "./images/kabirsingh.jpg",
+    albumSongs: [
+      {
+        songName: "Bekhayali",
+        artist: "Sachet Tandon",
+        songUrl: "./songs/Bekhayali - Kabir Singh.mp3",
+        songCoverImg: "./images/bekhayali.jpg",
+      },
+      {
+        songName: "Kaise Hua",
+        artist: "Vishal Sharma",
+        songUrl: "./songs/Kaise Hua - Kabir Singh.mp3",
+        songCoverImg: "./images/kaisehua.jpg",
+      },
+      {
+        songName: "Tujhe Kitna Chahne",
+        artist: "Arijit Singh",
+        songUrl: "./songs/Tujhe Kitna Chahne Lage - Kabir Singh.mp3",
+        songCoverImg: "./images/tujheKitna,jpg.jpg",
+      },
+      {
+        songName: "Mere Sohneya",
+        artist: "Sachet Tandon",
+        songUrl: "./songs/Mere Sohneya - Kabir Singh.mp3",
+        songCoverImg: "./images/meresohneya.jpg",
+      },
+      {
+        songName: "Tera Ban Jaunga",
+        artist: "Akhil Sachdeva",
+        songUrl: "./songs/Tera Ban Jaunga - Kabir Singh.mp3",
+        songCoverImg: "./images/teraBan.jpg",
+      },
+      {
+        songName: "Yeh Aaina",
+        artist: "Shreya Ghoshal",
+        songUrl: "./songs/Yeh Aaina - Kabir Singh.mp3",
+        songCoverImg: "./images/yehaaina.jpg",
+      },
+    ],
+  },
+
+  {
+    id: "a5830de7bec34d658d20c8799f514f1b",
+    albumArtist: "Vishal Dadlani",
+    album: "Pushpa",
+    albumArt: "./images/pushpa.jpg",
+    albumSongs: [
+      {
+        songName: "Jaago jaago Bakre",
+        artist: "Vishal Dadlani",
+        songUrl: "./songs/Jaago Jaago Bakre - Pushpa.mp3",
+        songCoverImg: "./images/jaagobakre.jpg",
+      },
+      {
+        songName: "Srivalli",
+        artist: "Javed Ali",
+        songUrl: "./songs/Srivalli - Pushpa.mp3",
+        songCoverImg: "./images/srivalli.jpg",
+      },
+      {
+        songName: "Oo Bolega Ya Oo",
+        artist: "Kanika Kapoor",
+        songUrl: "./songs/Oo Bolega Ya Oo Oo Bolega - Pushpa.mp3",
+        songCoverImg: "./images/oobolega.jpg",
+      },
+      {
+        songName: "Saami Saami",
+        artist: "Mounika Yadav",
+        songUrl: "./songs/Saami Saami Hindi - Pushpa.mp3",
+        songCoverImg: "./images/saami.jpg",
+      },
+    ],
+  },
+  {
+    id: "fea91c929859463090a384bda4919ea4",
+
+    albumArtist: "Javed Ali, Devi Sri Prasad",
+    album: "Kesari",
+    albumArt: "./images/kesari.jpg",
+    albumSongs: [
+      {
+        songName: "Ek Onkaar",
+        artist: "Jasbir Jassi",
+        songUrl: "./songs/Ek Onkaar - Kesari.mp3",
+        songCoverImg: "./images/ekonkaar.jpg",
+      },
+      {
+        songName: "Ve Maahi",
+        artist: "Arijit Singh",
+        songUrl: "./songs/Ve Maahi - Kesari.mp3",
+        songCoverImg: "./images/vemaahi.jpg",
+      },
+      {
+        songName: "Teri Mitti",
+        artist: "B Praak",
+        songUrl: "./songs/Teri Mitti - Kesari.mp3",
+        songCoverImg: "./images/terimitti.jpg",
+      },
+      {
+        songName: "Sanu Kehndi",
+        artist: "Romy, Brijesh Shandilya",
+        songUrl: "./songs/01 Sanu Kehndi - Kesari.mp3",
+        songCoverImg: "./images/sanukehndi.jpg",
+      },
+      {
+        songName: "Deh Shiva",
+        artist: "Jasleen Royal",
+        songUrl: "./songs/Deh Shiva Female - Kesari.mp3",
+        songCoverImg: "./images/dehsiva.jpg",
+      },
+    ],
+  },
+  {
+    id: "xea91c929859467956a384bda4919ea4",
+
+    albumArtist: "Pritam, Shilpa Rao",
+    album: "Ye Jawaani Hai Deewani",
+    albumArt: "./images/jawanidewaani.jpeg",
+    albumSongs: [
+      {
+        songName: "Subhanallah",
+        artist: "Sreeram",
+        songUrl: "./songs/Subhanallah (Yeh Jawaani Hai Deewani).mp3",
+        songCoverImg: "./images/jawanidewaani.jpeg",
+      },
+      {
+        songName: "IIahi",
+        artist: "Mohit Chauhan",
+        songUrl: "./songs/Ilahi (Yeh Jawaani Hai Deewani).mp3",
+        songCoverImg: "./images/jawanidewaani.jpeg",
+      },
+      {
+        songName: "Balam Pichkari",
+        artist: "Shilpa Rao",
+        songUrl:
+          "./songs/Balam Pichkari (Yeh Jawaani Hai Deewani).mp3",
+        songCoverImg: "./images/jawanidewaani.jpeg",
+      },
+      {
+        songName: "Badtameez Dil",
+        artist: "Benny Dayal",
+        songUrl:
+          "./songs/Badtameez Dil (Yeh Jawaani Hai Deewani).mp3",
+        songCoverImg: "./images/jawanidewaani.jpeg",
+      },
+    ],
+  },
+];
+
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+
+let result = musicData.map((item) => {
+  return item.albumSongs;
+});
+
+// console.log("1", result);
+
+let allSongsArray = [].concat.apply([], result);
+allSongsArray = shuffle(allSongsArray);
+// console.log("aS", allSongsArray);
+
+let allArtistsArray = allSongsArray.reduce(function (r, a) {
+  r[a.artist] = r[a.artist] || [];
+  r[a.artist].push(a);
+  return r;
+}, Object.create(null));
+
+allArtistsArray = Object.entries(allArtistsArray);
+
+// console.log("aAR", allArtistsArray);
+
+export { musicData, allArtistsArray, allSongsArray };

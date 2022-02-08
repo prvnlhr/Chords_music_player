8cf0452fe18c0da1f0b7bc0b347a03c60b5621d7
Praveen Lohar <praveenlohar.in@gmail.com>
2022-02-08 10:12:00 +0530

    added currList for songs

--- songsList.js
+++ songsList.js
@@ -41,36 +41,36 @@ const renderList = (dataIndex, dataArr, dataT) => {
   currPlayListDetails.from = dataT;
   currPlayListDetails.playListLength = dataArr.length;
 
-  const currList = dataArr;
-  currListHTML = currList
-    .map((item, index) => {
-      return `<div class="songWrapper">
+//   const currList = dataArr;
+//   currListHTML = currList
+//     .map((item, index) => {
+//       return `<div class="songWrapper">
   
-        <div class="songNoDiv">
-            <p>${index + 1 < 10 ? "0" + (index + 1) : index + 1}</p>
-        </div>
-        <div class="songArtDiv">
-        <img id="imgTag-SL" src=${item.songCoverImg} />
-        </div>
-        <div class="songNameDiv">
-            <p>${item.songName}</p>
-        </div>
-        <div class="songArtistNameDiv">
-            <p>${item.artist}</p>
-        </div>
-        <div class="songDurationDiv">
-        <svg class="svgIcon hideSvg" id="equalizer" width="40px" height="28px" viewBox="0 0 10 7" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <g fill="#860d8663">
-    <rect id="bar1" transform="translate(0.500000, 6.000000) rotate(180.000000) translate(-0.500000, -6.000000) " x="0" y="5" width="1" height="2px"></rect>
-    <rect id="bar2" transform="translate(3.500000, 4.500000) rotate(180.000000) translate(-3.500000, -4.500000) " x="3" y="2" width="1" height="5"></rect>
-    <rect id="bar3" transform="translate(6.500000, 3.500000) rotate(180.000000) translate(-6.500000, -3.500000) " x="6" y="0" width="1" height="7"></rect>
+//         <div class="songNoDiv">
+//             <p>${index + 1 < 10 ? "0" + (index + 1) : index + 1}</p>
+//         </div>
+//         <div class="songArtDiv">
+//         <img id="imgTag-SL" src=${item.songCoverImg} />
+//         </div>
+//         <div class="songNameDiv">
+//             <p>${item.songName}</p>
+//         </div>
+//         <div class="songArtistNameDiv">
+//             <p>${item.artist}</p>
+//         </div>
+//         <div class="songDurationDiv">
+//         <svg class="svgIcon hideSvg" id="equalizer" width="40px" height="28px" viewBox="0 0 10 7" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+//   <g fill="#860d8663">
+//     <rect id="bar1" transform="translate(0.500000, 6.000000) rotate(180.000000) translate(-0.500000, -6.000000) " x="0" y="5" width="1" height="2px"></rect>
+//     <rect id="bar2" transform="translate(3.500000, 4.500000) rotate(180.000000) translate(-3.500000, -4.500000) " x="3" y="2" width="1" height="5"></rect>
+//     <rect id="bar3" transform="translate(6.500000, 3.500000) rotate(180.000000) translate(-6.500000, -3.500000) " x="6" y="0" width="1" height="7"></rect>
      
-  </g>
-</svg>
-        </div>
-      </div> `;
-    })
-    .join("");
+//   </g>
+// </svg>
+//         </div>
+//       </div> `;
+//     })
+//     .join("");
 
   wrapper.innerHTML = currListHTML;
 
@@ -78,7 +78,7 @@ const renderList = (dataIndex, dataArr, dataT) => {
 
   for (let songIndex = 0; songIndex < songWrappers.length; songIndex++) {
     songWrappers[songIndex].addEventListener("click", (e) => {
-      // console.log("type", currPlayListDetails.from);
+      console.log("type", currPlayListDetails.from);
       let currSongData;
 
       if (currPlayListDetails.from === "songs") {
